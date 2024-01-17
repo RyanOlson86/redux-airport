e8e1d78687f3e74d38ea6f0c21bcce3b66db9f84
Ryan Olson <ryan.olson86@yahoo.com>
2024-01-17 14:56:06 -0600

    completed base goals

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,42 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  // Setup local state
+  const [airlineInput, setAirlineInput] = useState('')
+  // Setup store
+  const reduxStore = useSelector(store=>store)
+  // Setup dispatch
+  const dispatch = useDispatch()
+
+  // function to handle Add Airline button
+  const handleAddAirline = () => {
+    // console.log('clicked')
+    dispatch({type: 'ADD_AIRLINE', payload: airlineInput})
+    setAirlineInput('')
+  }
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <input 
+        placeholder='Airline Name'
+        type='text'
+        value={airlineInput}
+        onChange={(event)=>{setAirlineInput(event.target.value)}} 
+      />
+      <button onClick={handleAddAirline}>Add Airline</button>
+      
+      <table>
+        <tbody>
+          {reduxStore.map((airline, i)=>{
+            return (
+            <tr key={i}>
+              <td>{airline}</td>
+            </tr>
+          )})}
+        </tbody>
+      </table>
     </div>
   );
 }
